dfabb8ff63ec4942f8913138cc4aad501caaad32
viguime <jvictormguimaraes@gmail.com>
2021-07-09 21:19:44 -0300

    get and post methods

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { uuid, isUuid } = require('uuidv4');
 
 // const { v4: uuid, validate: isUuid } = require('uuid');
 
@@ -11,11 +12,23 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  const { title } = request.query;
+
+    const results = title
+        ? repositories.filter(repositories => repositories.title.includes(title))
+        : repositories;
+
+    return response.json(results);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {title,url, techs, likes} = request.body;
+
+  const repository = { id: uuid(), title, url, techs, likes};
+
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
