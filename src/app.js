a08450396a7b79d0169c9f86d3f3c9c798a83b32
viguime <jvictormguimaraes@gmail.com>
2021-07-10 21:17:43 -0300

    Post/id/like method and other fixes

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ app.get("/repositories", (request, response) => {
 app.post("/repositories", (request, response) => {
   const {title,url, techs, likes} = request.body;
 
-  const repository = { id: uuid(), title, url, techs, likes};
+  const repository = { id: uuid(), title, url, techs, likes:0};
 
   repositories.push(repository);
 
@@ -33,13 +33,15 @@ app.post("/repositories", (request, response) => {
 
 app.put("/repositories/:id", (request, response) => {
     const {id} = request.params;
-    const {title,url, techs, likes} = request.body;
+    const {title,url, techs} = request.body;
 
     const repositoryIndex = repositories.findIndex(repository => repository.id == id);
 
     if (repositoryIndex < 0 ) {
         return response.status(400).json({error: 'Repository not found'})
     }
+
+    likes = repositories[repositoryIndex].likes
 
     const repository = {
       id,
@@ -69,7 +71,32 @@ app.delete("/repositories/:id", (request, response) => {
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+
+  const {id} = request.params;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id == id);
+
+  if (repositoryIndex < 0 ) {
+      return response.status(400).json({error: 'Repository not found'})
+  }
+
+  const title = repositories[repositoryIndex].title
+  const url = repositories[repositoryIndex].url
+  const techs = repositories[repositoryIndex].techs
+  const likes = repositories[repositoryIndex].likes + 1
+
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes
+  };
+
+  repositories[repositoryIndex] = repository;
+
+  return response.json(repository);
+
 });
 
 module.exports = app;
